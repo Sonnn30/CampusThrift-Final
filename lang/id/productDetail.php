771203df4ed43ebc3e76b213430dd0bd05974969
Nama Kamu <emailgithubkamu@gmail.com>
2026-01-17 01:24:42 +0800

    fix: update Indonesian translation

--- lang/id/productDetail.php
+++ lang/id/productDetail.php
@@ -4,7 +4,7 @@ return [
     "Description" => "Deskripsi",
     "Shipping" => "Informasi Pengiriman",
     "Reviews" => "Penilaian",
-    "NoReview" => "Belum ada review",
+    "NoReview" => "Belum ada ulasan",
     "NoShipping" => "Belum ada metode pengiriman",
     "NoProduct" => "Tidak ada produk lain untuk ditampilkan.",
     "Another Item" => "Barang Lain"
